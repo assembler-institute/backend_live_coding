b124c2dec47d3eac9c9c33d625a1694d64b90e77
Alejandro Avila Pérez <43146182+alejandroaperez1994g@users.noreply.github.com>
2023-11-15 13:28:44 +0100

    Update client.ts

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,22 +1,22 @@
 import {PrismaClient as MongoClient} from "../../prisma/generated/mongodb_client";
 import {PrismaClient as PostgresClient} from "../../prisma/generated/postgresql_client";
 
+
 export const DATA_SOURCE = process.env.DATA_SOURCE ?? "mongo"
 
-interface ClientType {
-    [key: string]: MongoClient | PostgresClient;
-}
+type ClientMongo = MongoClient<Prisma.PrismaClientOptions, never, DefaultArgs>
+type ClientPostgres = PostgresClient<Prisma.PrismaClientOptions, never, DefaultArgs>
 
 export const mongoClient = new MongoClient();
 export const postgresClient = new PostgresClient();
 
-const clients: ClientType = {
-    mongo: mongoClient,
-    postgres: postgresClient,
-};
+export let clients: any
 
-export default clients[DATA_SOURCE as keyof ClientType];
-
+if (DATA_SOURCE === "postgres") {
+    clients = postgresClient
+} else {
+    clients = mongoClient
+}
 
 
 
